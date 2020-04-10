1200d236892e0b8a6c347ca42219ea6ca2f6f1d5
philospopher07 <norbusam@gmail.com>
2020-04-10 16:12:15 -0500

    submit button logged

--- scripts/clients.js
+++ scripts/clients.js
@@ -2,6 +2,11 @@
 $(document).ready(readyNow);
 
 function readyNow() {
-    
+    $('#submitButton').on('click', submit)
     
 }
+
+function submit() {
+    console.log('in submit');
+    
+}// end submit
