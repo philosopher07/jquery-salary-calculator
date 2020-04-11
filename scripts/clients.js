7c391445b31e6124c4abfcb3290189c8b5987e6f
philospopher07 <norbusam@gmail.com>
2020-04-11 10:10:08 -0500

    started work on displaying input to DOM

--- scripts/clients.js
+++ scripts/clients.js
@@ -6,8 +6,21 @@ function readyNow() {
     
 }
 
-let inventory = [];
+let employee = [];
 
+
+function displayEmployee() {
+    let el = $( 'employeeOut' )
+    // empty tr
+    el.empty();
+    // loop thru employee
+    for(let i=0; i<employee.length;i++){
+        // append each input to tr
+        el.append(`<td>
+        ${employee[ i ].firstname}
+        </td>`)
+    } //end for loop
+}// end displayEmployee
 
 
 function submit() {
@@ -19,7 +32,14 @@ function submit() {
         title: $('#titleIn').val(),
         annualSalary:$('#annualIn').val()
     }//end itemToAdd
-    inventory.push(itemToAdd);
-    //push value to inventory
+    //push value to employess
+    employee.push(itemToAdd);
     
+    //empty inputs
+    $('#firstIn').val('');
+    $('#lastIn').val('');
+    $('#idIn').val('');
+    $('#titleIn').val('');
+    $('#annualIn').val('');
+    displayEmployee();
 }// end submit
